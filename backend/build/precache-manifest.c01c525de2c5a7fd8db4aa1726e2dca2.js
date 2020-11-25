8bce63f317dd99a75f370aae35407a424b799235
Renier <pastelsunset@hotmail.com>
2020-11-25 14:48:30 +0200

    adding build

---
+++ backend/build/precache-manifest.c01c525de2c5a7fd8db4aa1726e2dca2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b37d5415eb9121286eb2",
+    "url": "/static/css/main.78121227.chunk.css"
+  },
+  {
+    "revision": "b37d5415eb9121286eb2",
+    "url": "/static/js/main.b37d5415.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f7b960c473afbac17141",
+    "url": "/static/js/2.f7b960c4.chunk.js"
+  },
+  {
+    "revision": "2f1e05c354c4f2e61e077c92fe92950b",
+    "url": "/static/media/musicapplogo.2f1e05c3.svg"
+  },
+  {
+    "revision": "0f408dcef7c1b1987c042645cfe42874",
+    "url": "/index.html"
+  }
+];
