29f8e73cfdd85f6298f99430ea2377915b2f484f
Renier <pastelsunset@hotmail.com>
2020-11-25 11:48:13 +0200

    commit of music app to heroku

---
+++ backend/README.md
@@ -0,0 +1,17 @@
+This Favourites Music App allows you to search the iTunes API for items and add them to a 'Favourites' list.
+
+How to use the App locally:
+1. Download the files to you local drive 
+2. Type 'npm install' in both the backend and frontend folders to install the dependencies and modules .
+3. In the backend folder type 'npm start' in the terminal to start the express server.
+4. In the frontend folder type 'npm start' in the terminal to start the React App. (listens at http://localhost:3000/)
+
+To Add titles to the favourites list:
+1. Search for a title of your choice
+2. Click the 'Add to favourites' button on the title to add it to your favourites list.
+
+To Remove titles from your favourites list:
+1. In your favourites list, click the 'Remove from Favourites' button to remove it from your favourites list.
+
+Security
+1. Helmet is used to secure the Express app by setting various HTTP headers
---
+++ backend/server.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const helmet = require("helmet");
+const app = express();
+const fileHandler = require('fs');
+const bodyParser = require('body-parser');
+
+require('isomorphic-fetch');
+
+app.use(helmet());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+
+// GET SEARCH RESULTS
+app.get('/api/search/:term/:media', function (req, res) {
+
+    var searchResults = {};
+
+    fetch(`https://itunes.apple.com/search?term=${req.params.term}&media=${req.params.media}`)
+        .then(res => res.json())
+        .then(
+            (result) => {
+                searchResults = result;
+                console.log(result);
+                res.send(searchResults);
+            },
+            (error) => {
+                console.log(error);
+            }
+
+        );
+
+})
+
+//GET SEARCH RESULTS - ONLY IF SEARCH IS EMPTY
+app.get('/api/search/', function (req, res) {
+
+    res.send({
+        "resultCount": 0,
+        "results": []
+    })
+
+})
+
+// GET FAVOURITES
+app.get('/api/favourites', function (req, res) {
+    fileHandler.readFile('favourites.json', (err, data) => {
+        if (err) res.send("No Web Projects Found.");
+        else
+            res.send(data);
+    })
+})
+
+// ADD TO FAVOURITES
+app.post('/api/favourites/', function (req, res) {
+
+
+    fileHandler.readFile('favourites.json', (err, data) => {
+        if (err) res.send("No Favourites Found.");
+        else
+            var allFavourites = {};
+        allFavourites = JSON.parse(data);
+
+        console.log(req.body);
+
+        allFavourites.favourites.unshift(req.body);
+
+        fileHandler.writeFile('favourites.json', JSON.stringify(allFavourites), (err) => {
+            if (err) throw err;
+            res.send(allFavourites);
+        })
+    })
+
+})
+
+// REMOVE TRACK FROM FAVOURITES LIST
+app.delete('/api/favourites/:trackId', function (req, res) {
+
+    fileHandler.readFile('favourites.json', (err, data) => {
+        if (err) res.send("No Track with that ID was Found.");
+        else
+            var favouritesList = {};
+        favouritesList = JSON.parse(data);
+
+
+
+        for (i = 0; i < favouritesList.favourites.length; i++) {
+
+            if (favouritesList.favourites[i].trackId == req.params.trackId) {
+                favouritesList.favourites.splice(i, 1);
+            }
+        }
+
+        fileHandler.writeFile('favourites.json', JSON.stringify(favouritesList), (err) => {
+            if (err) throw err;
+            res.send("Project removed");
+        })
+        res.json(favouritesList);
+    })
+
+})
+
+
+app.use(function (err, req, res, next) {
+    console.log(err.stack)
+    res.status(500).send('Something broke!')
+})
+
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
